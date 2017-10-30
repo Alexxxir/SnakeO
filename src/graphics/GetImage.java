dbdf82fef01b692a98028a3f43b381a4d40944f3
Алексей <ignatenko.a.e@yandex.ru>
2017-10-30 17:39:35 +0300

    fdsd

--- src/graphics/GetImage.java
+++ src/graphics/GetImage.java
@@ -3,7 +3,6 @@ package graphics;
 import static java.awt.image.AffineTransformOp.TYPE_BILINEAR;
 
 import java.awt.Image;
-import java.awt.List;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
@@ -20,6 +19,7 @@ import javax.swing.Timer;
 
 import game.Direction;
 import game.MapOfDirections;
+import objects.ObjectOnField;
 import objects.PieceOfSnake;
 
 public class GetImage {
@@ -28,6 +28,16 @@ public class GetImage {
 	
     public Map<String, Map<Direction, Image>> images = new HashMap<String, Map<Direction, Image>>();
     private MapOfDirections mapOfDirections = new MapOfDirections();
+    
+    public Image getImage(ObjectOnField objectOnField, int counter) {
+    	Direction direction = Direction.Right;
+    	if (objectOnField instanceof PieceOfSnake) {
+    		PieceOfSnake pieceOfSnake = (PieceOfSnake) objectOnField;
+    		direction = pieceOfSnake.direction;
+    	}
+		String obj = objectOnField.nameOfTheObject();
+		return images.get(animationNameOfTheObject(obj, counter)).get(direction);
+    }
     
     private BufferedImage rotateImage(BufferedImage img, int angle)
     {
@@ -40,95 +50,23 @@ public class GetImage {
     
     public void initDirectionsOfSnake() 
     {
-        this.mapOfDirections.addDoubleDirections(Direction.Down, Direction.Right, Direction.Right);
-        this.mapOfDirections.addDoubleDirections(Direction.Down, Direction.Left, Direction.Up);
-        this.mapOfDirections.addDoubleDirections(Direction.Up, Direction.Right, Direction.Down);
-        this.mapOfDirections.addDoubleDirections(Direction.Right, Direction.Down, Direction.Left);
+        this.mapOfDirections.add(Direction.Down, Direction.Right, Direction.Right);
+        this.mapOfDirections.add(Direction.Up, Direction.Left, Direction.Right);
+        this.mapOfDirections.add(Direction.Down, Direction.Left, Direction.Up);
+        this.mapOfDirections.add(Direction.Up, Direction.Right, Direction.Up);
+        this.mapOfDirections.add(Direction.Up, Direction.Right, Direction.Down);
+        this.mapOfDirections.add(Direction.Down, Direction.Left, Direction.Down);
+        this.mapOfDirections.add(Direction.Right, Direction.Down, Direction.Left);
+        this.mapOfDirections.add(Direction.Left, Direction.Up, Direction.Left);
     }
+   
     
-   /* public String animationNameOfTheObjectSnake(PieceOfSnake snakePart, int counter) 
+    public String animationNameOfTheObject(String objectName, int counter) 
     {
-	    if (snakePart.lastPiece == null) {
-	    	if (counter % 2 == 0)
-	    		return "SnakeTail0";
-	    	return "SnakeTail1";
-	    }
-	    if (snakePart.nextPiece == null) {
-	    	if (counter % 2 == 0)
-	    		return "SnakeHead0";
-	    	return "SnakeHead1";
-	    }
-	    if (snakePart.nextPiece.direction != snakePart.direction)
-	        return "SnakeTwist0";
-	    return "SnakePart0";
-    } */
-    
-    public String animationNameOfTheObjectSnake(String snakePart, int counter) 
-    {
-    	snakePart = GetName(snakePart);   		
-    	
-	    if (snakePart == "SnakeTail") {
-	    	if (counter % 2 == 0)
-	    		return "SnakeTail0";
-	    	return "SnakeTail1";
-	    }
-	    if (snakePart == "SnakeHead") {
-	    	if (counter % 2 == 0)
-	    		return "SnakeHead0";
-	    	return "SnakeHead1";
-	    }
-	    if (snakePart == "SnakeTwist")
-	        return "SnakeTwist0";
-	    return "SnakePart0";
+    	if (animationImages.containsKey(objectName))
+    		return animationImages.get(objectName)[counter % animationImages.get(objectName).length];
+    	return "Undefined0";
     }
-    
-    public String animationNameOfTheObjectOther(String obj, int counter) 
-    {
-    	obj = GetName(obj);
-	    if (obj == "Wall") {
-	    	if (counter % 2 == 0)
-	    		return "Wall0";
-	    	return "Wall0";
-	    }
-	    if (obj == "EmptySpace") {
-	    	if (counter % 2 == 0)
-	    		return "EmptySpace0";
-	    	return "EmptySpace0";
-	    }
-	    if (obj == "Apple") {
-	    	if (counter % 2 == 0)
-	    		return "Apple0";
-	    	return "Apple0";
-	    }
-	    if (obj == "Cherry") {
-	    	if (counter % 2 == 0)
-	    		return "Cherry0";
-	    	return "Cherry0";
-	    }
-	    if (obj == "Clock") {
-	    	if (counter % 2 == 0)
-	    		return "Clock0";
-	    	return "Clock0";
-	    }
-	    return "Undefind";
-    }
-    
-    public String GetName(String word)
-    {
-    	int number = 0;
-    	for (int i = 0; i < word.length(); i++)
-    	{
-    		try 
-    		{
-    	        number = Character.getNumericValue(word.charAt(i));
-    	        return word.substring(0, i);
-    	    } 
-    		catch (NumberFormatException e) 
-    		{
-    	    }	    		
-    	}
-    	return "None";
-    }  
 	
 	public void initAnimation()
 	{
@@ -136,7 +74,8 @@ public class GetImage {
 		animationImages.put("SnakeHead", new String[] {"SnakeHead0", "SnakeHead1"});
 		animationImages.put("SnakeTwist", new String[] {"SnakeTwist0"});
 		animationImages.put("SnakePart", new String[] {"SnakePart0"});
-		animationImages.put("Wall", new String[] {"Wall0"});
+		animationImages.put("Wall", new String[] {"Wall0", "Wall1", "Wall2", "Wall3", "Wall4", "Wall5", "Wall6", "Wall7", "Wall8", "Wall9", "Wall10"});
+		animationImages.put("RottenApple", new String[] {"RottenApple0"});
 		animationImages.put("EmptySpace", new String[] {"EmptySpace0"});
 		animationImages.put("Clock", new String[] {"Clock0"});
 		animationImages.put("Cherry", new String[] {"Cherry0"});
@@ -147,7 +86,6 @@ public class GetImage {
 			
 			for (int i = 0; i < animationImages.get(str).length; i++) 
 			{
-				System.out.println(animationImages.get(str)[i]);
 	            BufferedImage imgHead;
 				try {
 					imgHead = ImageIO.read(
@@ -159,8 +97,7 @@ public class GetImage {
 		            map1.put(Direction.Right, rotateImage(imgHead, 0));
 		            this.images.put(animationImages.get(str)[i], map1);
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					System.out.println(e);
 				}
 	        }
 		}
--- src/graphics/Gui.java
+++ src/graphics/Gui.java
@@ -24,87 +24,6 @@ public class Gui extends JPanel implements ActionListener{
     private final int fps = 8;
     private Timer timer = new Timer(1000/fps, this);
 	private GetImage paintImage;
-    
- /*   private String[] typesOfPieces = {"SnakeTail",
-									  "SnakeTail2",
-							          "SnakeHead",
-							          "SnakeHead2",
-							          "SnakeTwist",
-							          "SnakePart"};
-
-    private void initListOfImages() throws IOException {
-        for (String typeOfPiece: this.typesOfPieces) {
-            BufferedImage imgHead = ImageIO.read(
-                    new File(String.format("images/%s.png", typeOfPiece)));
-            Map<Direction, Image> map = new HashMap<Direction, Image>();
-            map.put(Direction.Down, rotateImage(imgHead, 90));
-            map.put(Direction.Left, rotateImage(imgHead, 180));
-            map.put(Direction.Up, rotateImage(imgHead, 270));
-            map.put(Direction.Right, rotateImage(imgHead, 0));
-            this.images.put(typeOfPiece, map);
-        }
-    } */
-
-  /*  private void initDirectionsOfSnake() {
-        this.mapOfDirections = new MapOfDirections();
-        this.mapOfDirections.addDoubleDirections(Direction.Down, Direction.Right, Direction.Right);
-        this.mapOfDirections.addDoubleDirections(Direction.Down, Direction.Left, Direction.Up);
-        this.mapOfDirections.addDoubleDirections(Direction.Up, Direction.Right, Direction.Down);
-        this.mapOfDirections.addDoubleDirections(Direction.Right, Direction.Down, Direction.Left);
-    } */
-    
- /*   public String animationNameOfTheObject(PieceOfSnake snakePart, int counter) {
-        if (snakePart.lastPiece == null) {
-        	if (counter % 2 == 0)
-        		return "SnakeTail";
-        	return "SnakeTail2";
-        }
-        if (snakePart.nextPiece == null) {
-        	if (counter % 2 == 0)
-        		return "SnakeHead";
-        	return "SnakeHead2";
-        }
-        if (snakePart.nextPiece.direction != snakePart.direction)
-            return "SnakeTwist";
-        return "SnakePart";
-    } */
-
-    private Image getImage(ObjectOnField objectOnField) {
-    	if (objectOnField instanceof PieceOfSnake) 
-    	{
-    		PieceOfSnake pieceOfSnake = (PieceOfSnake) objectOnField;
-    		return paintImage.images.get(paintImage.animationNameOfTheObjectSnake(objectOnField.nameOfTheObject(), counter)).get(pieceOfSnake.direction);
-    	}
-    	else
-    	{
-    		String obj = objectOnField.nameOfTheObject();
-    		return paintImage.images.get(paintImage.animationNameOfTheObjectOther(obj, counter)).get(Direction.None);
-    	}
-    	
-     /*   if (!(objectOnField instanceof PieceOfSnake)) {
-            Image image = new ImageIcon(String.format("images/%s.png", objectOnField.nameOfTheObject())) .getImage();
-            if (image.getHeight(null) != -1) {
-                return image;
-            } else {
-                return new ImageIcon(String.format("images/Undefined.png", objectOnField.nameOfTheObject())) .getImage();
-            }
-        } else {
-        	PieceOfSnake pieceOfSnake = (PieceOfSnake) objectOnField;
-            if (objectOnField.nameOfTheObject() == "SnakeHead")
-                return this.images.get(animationNameOfTheObject(pieceOfSnake, counter)).get(pieceOfSnake.direction);
-            if (objectOnField.nameOfTheObject() == "SnakeTail")
-            	return this.images.get(animationNameOfTheObject(pieceOfSnake, counter)).get(pieceOfSnake.nextPiece.direction);
-            if (objectOnField.nameOfTheObject() == "SnakeTwist") {                
-                this.initDirectionsOfSnake();
-                return this.images.get(animationNameOfTheObject(pieceOfSnake, counter)).get(
-                        this.mapOfDirections.get(pieceOfSnake.nextPiece.direction, pieceOfSnake.direction)
-                );
-            }
-            if (objectOnField.nameOfTheObject() == "SnakePart") 
-                return this.images.get(animationNameOfTheObject(pieceOfSnake, counter)).get(pieceOfSnake.direction);                
-            return this.images.get(animationNameOfTheObject(pieceOfSnake, counter)).get(Direction.None);
-        } */
-    }
 
     public Gui() throws IOException {
     	this.paintImage = new GetImage();
@@ -128,7 +47,7 @@ public class Gui extends JPanel implements ActionListener{
     public void paint(Graphics g) {
         for (int x = 0; x < this.field.getLengthX(); x++){
             for (int y = 0; y < this.field.getLengthY(); y++){
-                ((Graphics2D)g).drawImage(new ImageIcon("images/EmptySpace.png").getImage(),
+                ((Graphics2D)g).drawImage(new ImageIcon("images/EmptySpace0.png").getImage(),
                         x * cellWidth(),
                         y * cellHeight(),
                         cellWidth(),
@@ -139,7 +58,7 @@ public class Gui extends JPanel implements ActionListener{
         for (int x = 0; x < this.field.getLengthX(); x++) {
             for (int y = 0; y < this.field.getLengthY(); y++) {
                 Image img = null;
-                img = getImage(this.field.getObjectOnField(new Coordinate(x, y)));
+                img = paintImage.getImage(this.field.getObjectOnField(new Coordinate(x, y)), counter);
                 ((Graphics2D)g).drawImage(img,
                         x * cellWidth(),
                         y * cellHeight(),
